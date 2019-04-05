d138ea58728b416bb0a691cc8b9f4edc3602ee13
David Garza <dgarza0413@gmail.com>
2019-04-04 22:52:00 -0500

    scroll behavior add css

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var port = 3000;
 
-mongoose.connect("mongodb://localhost/portfolio");
+//mongoose.connect("mongodb://localhost/portfolio");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
+
+var Url = "http://localhost:3000/";
 
 app.get("/", function(req, res) {
   res.render("home");
@@ -16,6 +19,9 @@ app.get("/", function(req, res) {
 app.post("");
 
 //must change to different port
-app.listen(process.env.PORT, process.env.IP, function() {
-  console.log("server is listening");
-});
+app.listen(port, () =>
+  console.log(`server is listening on port ${port}! on ${Url}`)
+);
+// app.listen(process.env.PORT, process.env.IP, function() {
+//   console.log("server is listening");
+// });
